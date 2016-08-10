109c74c736f008c33cf2de7ca7df33076e2645fe
Alex <alexandr.pa@livetex.ru>
2016-08-10 15:51:12 +0300

    some improvies

--- src/checker.cc
+++ src/checker.cc
@@ -4,7 +4,7 @@
 using namespace v8;
 
 namespace checker {
-  int lastInputTime() {
+  int64_t lastInputTime() {
     LASTINPUTINFO li;
     li.cbSize = sizeof(LASTINPUTINFO);
     ::GetLastInputInfo(&li);
@@ -12,28 +12,52 @@ namespace checker {
     return ::GetTickCount() - li.dwTime;
   }
 
+  bool isUserActive(int64_t delay, int64_t lit) {
+    if (delay >= lit) {
+      return true;
+    }
+    return false;
+  }
+
+  bool checkArguments(const FunctionCallbackInfo<Value>& args) {
+    if (args.Length() == 2 && args[0]->IsNumber() && args[1]->IsFunction()) {
+      return true;
+    }
+    return false;
+  }
+
+  void userActivitySpy(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+    bool userActive = true;
+    int64_t delay = args[0]->IntegerValue();
+    Local<Function> callback = Local<Function>::Cast(args[1]);
+
+    do {
+      userActive = isUserActive(delay, lastInputTime());
+      if(!userActive) {
+        const unsigned argc = 1;
+        Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "user gone") };
+        callback->Call(Null(isolate), argc, argv);
+      }
+    } while(userActive);
+  }
+
   /**
    * @param { int } args[0] время простоя
    * @param { function } args[1] js callback
    */
   void method(const FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = args.GetIsolate();
-    bool userActive = true;
-    int delay = Integer::New(args[0]);
-    Local<Function> callback = Local<Function>::Cast(args[1]);
-
-    do {
-      userActive = delay <= lastInputTime();
-      if(!userActive) {
-        callback->Call(Null(isolate));
-      }
-    } while(userActive);
-
-    // args.GetReturnValue().Set(Integer::New(isolate, checker::lastInputTime()));    
+    if (checkArguments(args)) {
+      userActivitySpy(args);
+    } else {
+      Isolate* isolate = args.GetIsolate();
+      isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    }
   }
 
   void init(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "checkDwTime",  method);
+    NODE_SET_METHOD(exports, "ifUserGone",  method);
   }
 
   NODE_MODULE(checker, init)
