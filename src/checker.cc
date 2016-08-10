ec2db6cb5e16b906d719ba5de603a7b0d5f8fdcf
Alex <alexandr.pa@livetex.ru>
2016-08-10 14:14:49 +0300

    check updates

--- src/checker.cc
+++ src/checker.cc
@@ -12,15 +12,29 @@ namespace checker {
     return ::GetTickCount() - li.dwTime;
   }
 
+  /**
+   * @param { int } args[0] время простоя
+   * @param { function } args[1] js callback
+   */
   void method(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
-    args.GetReturnValue().Set(Integer::New(isolate, checker::lastInputTime()));
+    bool userActive = true;
+    int delay = Integer::New(args[0]);
+    Local<Function> callback = Local<Function>::Cast(args[1]);
+
+    do {
+      userActive = delay <= lastInputTime();
+      if(!userActive) {
+        callback->Call(Null(isolate));
+      }
+    } while(userActive);
+
+    // args.GetReturnValue().Set(Integer::New(isolate, checker::lastInputTime()));    
   }
 
   void init(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "checkFreeTime",  method);
+    NODE_SET_METHOD(exports, "checkDwTime",  method);
   }
 
   NODE_MODULE(checker, init)
 }
-
