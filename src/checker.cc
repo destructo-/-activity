1bdf7c09bfff2895e2c31bc0363327401d7ec399
Alex <alexandr.pa@livetex.ru>
2016-08-11 16:48:48 +0300

    async

--- src/checker.cc
+++ src/checker.cc
@@ -1,13 +1,13 @@
-#include <node.h>
+#define _WINSOCKAPI_
+
+#include <nan.h>
 #include <windows.h>
+#include <node.h>
 
 using namespace v8;
+using namespace Nan;
 
-namespace checker {
-  void spyActiveUser(const FunctionCallbackInfo<Value>& args);
-  void spyUnactiveUser(const FunctionCallbackInfo<Value>& args);
-
-  int64_t lastInputTime() {
+int lastInputTime() {
     LASTINPUTINFO li;
     li.cbSize = sizeof(LASTINPUTINFO);
     ::GetLastInputInfo(&li);
@@ -15,74 +15,89 @@ namespace checker {
     return ::GetTickCount() - li.dwTime;
   }
 
-  bool isUserActive(int64_t delay, int64_t lit) {
-    if (delay >= lit) {
-      return true;
-    }
-    return false;
+bool isUserActive(int delay, int lit) {
+  if (delay >= lit) {
+    return true;
   }
+  return false;
+}
 
-  bool checkArguments(const FunctionCallbackInfo<Value>& args) {
-    if (args.Length() == 3 && args[0]->IsNumber() &&
-        args[1]->IsFunction() && args[2]->IsFunction()) {
-      return true;
-    }
-    return false;
-  }
-
-  void spyUnactiveUser(const FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = args.GetIsolate();
-    bool userActive = false;
-    int64_t delay = args[0]->IntegerValue();
-    Local<Function> callback = Local<Function>::Cast(args[2]);
-
-    do {
-      userActive = isUserActive(delay, lastInputTime());
-      if(userActive) {
-        const unsigned argc = 1;
-        Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "user back") };
-        callback->Call(Null(isolate), argc, argv);
-      }
-    } while(!userActive);
-
-    spyActiveUser(args);
-  }
-
-  void spyActiveUser(const FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = args.GetIsolate();
+int spyActiveUser(int delay) {
     bool userActive = true;
-    int64_t delay = args[0]->IntegerValue();
-    Local<Function> callback = Local<Function>::Cast(args[1]);
 
     do {
       userActive = isUserActive(delay, lastInputTime());
       if(!userActive) {
-        const unsigned argc = 1;
-        Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "user gone") };
-        callback->Call(Null(isolate), argc, argv);
+        return 42;
       }
     } while(userActive);
-
-    spyUnactiveUser(args);
-  }
-
-  /**
-   * @param { int } args[0] время простоя
-   * @param { function } args[1] js callback
-   */
-  void method(const FunctionCallbackInfo<Value>& args) {
-    if (checkArguments(args)) {
-      spyActiveUser(args);
-    } else {
-      Isolate* isolate = args.GetIsolate();
-      isolate->ThrowException(Exception::TypeError(
-        String::NewFromUtf8(isolate, "Wrong arguments")));
-    }
-  }
-
-  void init(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "userActivitySpy",  method);
-  }
-
-  NODE_MODULE(checker, init)
 }
+
+int spyUnactiveUser(int delay) {
+    bool userActive = false;
+
+    do {
+      userActive = isUserActive(delay, lastInputTime());
+      if(userActive) {
+        return 42;
+      }
+    } while(!userActive);
+}
+
+class SpyWorker : public AsyncWorker {
+ public:
+  SpyWorker(int delay, Callback *callback, int (*asyncMethod)(int))
+    : AsyncWorker(callback), delay(delay), estimate(0), asyncMethod(asyncMethod)  {}
+  ~SpyWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures
+  // here, so everything we need for input and output
+  // should go on `this`.
+  void Execute () {
+    estimate = asyncMethod(delay);
+  }
+
+  // Executed when the async work is complete
+  // this function will be run inside the main event loop
+  // so it is safe to use V8 again
+  void HandleOKCallback () {
+    Nan::HandleScope scope;
+
+    Local<Value> argv[] = {
+        Null()
+      , New<Number>(estimate)
+    };
+
+    callback->Call(2, argv);
+  }
+
+ private:
+  int delay;
+  int estimate;
+  int (*asyncMethod)(int);
+};
+
+NAN_METHOD(AsyncSpyActive) {
+  int points = To<int>(info[0]).FromJust();
+  Callback *callback = new Callback(info[1].As<Function>());
+
+  AsyncQueueWorker(new PiWorker(points, callback, &spyActiveUser));
+}
+
+NAN_METHOD(AsyncSpyUnactive) {
+  int points = To<int>(info[0]).FromJust();
+  Callback *callback = new Callback(info[1].As<Function>());
+
+  AsyncQueueWorker(new PiWorker(points, callback, &spyUnactiveUser));
+}
+
+NAN_MODULE_INIT(InitAll) {
+  Nan::Set(target, New<String>("spyActiveUser").ToLocalChecked(),
+    GetFunction(New<FunctionTemplate>(AsyncSpyActive)).ToLocalChecked());
+
+  Nan::Set(target, New<String>("spyUnactiveUser").ToLocalChecked(),
+    GetFunction(New<FunctionTemplate>(AsyncSpyUnactive)).ToLocalChecked());
+}
+
+NODE_MODULE(addon, InitAll)
