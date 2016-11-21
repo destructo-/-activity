1a87c86cc6437b8cdf302457345916e74ac3c6c4
Cerfuck <ivadik05@gmail.com>
2016-11-21 17:03:04 +0300

    fix mac activate window

---
+++ src/activate-window/activate_window_mac.cc
@@ -0,0 +1,16 @@
+#define _WINSOCKAPI_
+
+#include "activate_window.h"
+#include <nan.h>
+#include <node.h>
+
+using namespace v8;
+using namespace Nan;
+
+NAN_METHOD(ActivateWindow) {
+
+}
+
+NAN_METHOD(SetActiveWindow) {
+
+}
