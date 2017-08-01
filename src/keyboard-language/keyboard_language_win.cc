d01c15b7ff42ab275cfff46539e88602e80d45dd
Cerfuck <ivadik05@gmail.com>
2017-08-01 16:55:08 +0300

    delete

--- src/keyboard-language/keyboard_language_win.cc
+++ src/keyboard-language/keyboard_language_win.cc
@@ -1,7 +1,6 @@
 #define _WINSOCKAPI_
 
 #include "keyboard_language.h"
-#include "LanguageProfileNotifySink.h"
 #include <nan.h>
 #include <windows.h>
 #include <node.h>
@@ -10,10 +9,5 @@ using namespace v8;
 using namespace Nan;
 
 NAN_METHOD(GetCurrentKeyboardLanguage) {
-  Local<Function> callbackHandle = info[0].As<Function>();
-  Nan::Callback *callback = new Nan::Callback(callbackHandle);
-  HRESULT CLanguageProfileNotifySink::OnLanguageChanged() {
-    callback();
-    return S_OK;
-  }
+
 }
